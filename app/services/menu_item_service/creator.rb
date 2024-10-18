1095299e8a7dcf1b8ba1b2d19566615b310b450d
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-18 17:16:42 -0300

    MenuItem service creator
    - Implement service creator tests
    - Implement service creator class that handle MenuItem creation

---
+++ app/services/menu_item_service/creator.rb
@@ -0,0 +1,17 @@
+class MenuItemService::Creator
+  def initialize(params)
+    @menu_id = params[:menu_id]
+    @name = params[:name]
+    @description = params[:description]
+    @price = params[:price]
+  end
+  
+  def execute
+    MenuItem.create!(
+      name: @name,
+      description: @description,
+      price: @price,
+      menu_id: @menu_id
+    )
+  end
+end
---
+++ spec/services/menu_item_service/creator_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe MenuItemService::Creator do
+  describe ".execute" do
+    let!(:menu) { Menu.create(title: 'Burguers') }
+
+    subject { described_class.new(params).execute }
+
+    context "with required attributes" do
+      let(:params) {
+        {
+          name: 'Big Mac',
+          description: 'Buns, patties, cheese, lettuce pickles, onions, sauce, paprika',
+          price: 5.69,
+          menu_id: menu.id
+        }
+      }
+
+      it "creates a new menu item for menu" do
+        expect{ subject }.to change { MenuItem.count}.by 1
+      end
+    end
+
+    context "without required attributes" do
+      let(:params) { { menu_id: menu.id } }
+      
+      it "do not creates a new menu item for menu" do
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end 
+  end
+end
