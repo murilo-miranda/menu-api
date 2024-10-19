fd28de072b9fb4c0856b0535f0e8d37b76c26f21
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-19 18:14:26 -0300

    Many to Many
    - Add many to many relation to handle MenuItem be on multiple Menus
    - Remove null validation of menu_id from MenuItem due to rails handles via many-to-many

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,5 +1,6 @@
 class MenuItem < ApplicationRecord
-  belongs_to :menu
+  has_many :menu_items_menus
+  has_many :menus, through: :menu_items_menus
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
---
+++ db/migrate/20241019203220_create_menu_items_menus.rb
@@ -0,0 +1,10 @@
+class CreateMenuItemsMenus < ActiveRecord::Migration[7.2]
+  def change
+    create_table :menu_items_menus do |t|
+      t.references :menu, null: false, foreign_key: true
+      t.references :menu_item, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
