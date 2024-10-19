eb9ac9e7106ea31c04efba23069fa082c5abdb5a
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-19 13:52:16 -0300

    Add uniqueness
    - Add uniqueness to database level
    - Add uniqueness to rails level
    - Implement model duplicated test scenario

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 describe MenuItem, type: :model do
+  let!(:menu) { Menu.create(title: 'Burguers') }
+
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:price) }
+    subject { MenuItem.new(name: 'Big Mac', price: 5.69, menu_id: menu.id) }
+    it { should validate_uniqueness_of(:name) }
   end
 
   describe "association" do
