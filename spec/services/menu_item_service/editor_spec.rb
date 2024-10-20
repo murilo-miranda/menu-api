b5050479e893ee879f2e799e894fd8a45dba98b6
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-19 22:58:34 -0300

    Refactor editor
    - Make use of strong parameters to update record instead of using initialize obj
    - Try to make test more descritive

--- spec/services/menu_item_service/editor_spec.rb
+++ spec/services/menu_item_service/editor_spec.rb
@@ -4,36 +4,49 @@ require 'rails_helper'
 
 describe MenuItemService::Editor do
   describe ".execute" do
+    let(:old_name) { 'Big Mac' }
+    let(:old_description) { 'Buns, patties, cheese, lettuce pickles, onions, sauce, paprika' }
+    let(:old_price) { 5.69 }
+    let(:old_menu_ids) { [ menu.id ] }
+    let(:old_menus) { menu }
+    let(:new_name) { 'The Classic' }
+    let(:new_description) { 'Buns, patties, chopped onions, ketchup, mustard' }
+    let(:new_price) { 2.19 }
+    let(:new_menu_ids) { [ menu2.id ] }
+    let(:new_menus) { menu2 }
+
     let!(:menu) { Menu.create(title: "Burguers") }
+    let!(:menu2) { Menu.create(title: "Lunch") }
     let!(:menu_item) {
       MenuItem.create(
-        name: 'Big Mac',
-        description: 'Buns, patties, cheese, lettuce pickles, onions, sauce, paprika',
-        price: 5.69,
-        menu_id: menu.id
+        name: old_name,
+        description: old_description,
+        price: old_price,
+        menu_ids: old_menu_ids
       )
     }
 
-    context "existed record" do
-      context "with required params" do
-        subject { described_class.new(params).execute }
+    subject { described_class.new(params).execute }
 
+    context "existed menu item" do
+      context "with all params" do
         let(:params) {
           {
             id: menu_item.id,
-            name: 'The Classic',
-            description: 'Buns, patties, chopped onions, ketchup, mustard',
-            price: 2.19,
-            menu_id: menu.id
+            name: new_name,
+            description: new_description,
+            price: new_price,
+            menu_ids: new_menu_ids
           }
         }
 
-        it "updates informed menu" do
+        it "updates informed menu item" do
           subject
           expect(menu_item.reload.id).to eq(params.fetch(:id))
-          expect(menu_item.reload.name).to eq("The Classic")
-          expect(menu_item.reload.description).to eq("Buns, patties, chopped onions, ketchup, mustard")
-          expect(menu_item.reload.price).to eq(2.19)
+          expect(menu_item.reload.name).to eq(new_name)
+          expect(menu_item.reload.description).to eq(new_description)
+          expect(menu_item.reload.price).to eq(new_price)
+          expect(menu_item.reload.menus).to contain_exactly(new_menus)
         end
 
         context "with name already used" do
@@ -41,8 +54,7 @@ describe MenuItemService::Editor do
             MenuItem.create(
               name: 'The Classic',
               description: 'Buns, patties, chopped onions, ketchup, mustard',
-              price: 2.19,
-              menu_id: menu.id
+              price: 2.19
             )
           }
 
@@ -51,8 +63,7 @@ describe MenuItemService::Editor do
               id: new_menu_item.id,
               name: 'Big Mac',
               description: 'Buns, patties, cheese, lettuce pickles, onions, sauce, paprika',
-              price: 5.69,
-              menu_id: menu.id
+              price: 5.69
             }
           }
 
@@ -66,19 +77,77 @@ describe MenuItemService::Editor do
         end
       end
 
-      context "without required params" do
-        subject { described_class.new(params).execute }
-
-        let(:params) {
-          {
-            id: menu_item.id
+      context "with only params" do
+        context 'name' do
+          let(:params) {
+            {
+              id: menu_item.id,
+              name: 'New name'
+            }
           }
-        }
+          
+          it "only updates the name of informed menu item" do
+            subject
+            expect(menu_item.reload.id).to eq(params.fetch(:id))
+            expect(menu_item.reload.name).to eq("New name")
+            expect(menu_item.reload.description).to eq(old_description)
+            expect(menu_item.reload.price).to eq(old_price)
+            expect(menu_item.reload.menus).to contain_exactly(old_menus)
+          end
+        end
 
-        it "do not updates informed menu" do
-          expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
-          expect(menu_item.reload.id).to eq(params.fetch(:id))
-          expect(menu_item.reload.name).to eq("Big Mac")
+        context 'description' do
+          let(:params) {
+            {
+              id: menu_item.id,
+              description: new_description
+            }
+          }
+          
+          it "only updates the description of informed menu item" do
+            subject
+            expect(menu_item.reload.id).to eq(params.fetch(:id))
+            expect(menu_item.reload.name).to eq(old_name)
+            expect(menu_item.reload.description).to eq(new_description)
+            expect(menu_item.reload.price).to eq(old_price)
+            expect(menu_item.reload.menus).to contain_exactly(old_menus)
+          end
+        end
+
+        context 'price' do
+          let(:params) {
+            {
+              id: menu_item.id,
+              price: new_price
+            }
+          }
+          
+          it "only updates the price of informed menu item" do
+            subject
+            expect(menu_item.reload.id).to eq(params.fetch(:id))
+            expect(menu_item.reload.name).to eq(old_name)
+            expect(menu_item.reload.description).to eq(old_description)
+            expect(menu_item.reload.price).to eq(new_price)
+            expect(menu_item.reload.menus).to contain_exactly(old_menus)
+          end
+        end
+
+        context 'menu_ids' do
+          let(:params) {
+            {
+              id: menu_item.id,
+              menu_ids: new_menu_ids
+            }
+          }
+          
+          it "only updates the menu_ids of informed menu item" do
+            subject
+            expect(menu_item.reload.id).to eq(params.fetch(:id))
+            expect(menu_item.reload.name).to eq(old_name)
+            expect(menu_item.reload.description).to eq(old_description)
+            expect(menu_item.reload.price).to eq(old_price)
+            expect(menu_item.reload.menus).to contain_exactly(new_menus)
+          end
         end
       end
     end
