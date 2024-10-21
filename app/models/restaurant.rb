d9d67e9ba2f28a50f0ecf158f36e2f598c540d52
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-21 07:19:25 -0300

    Fix destroy model
    - Fix for restaurant destroy method, the association throw error about foreign key

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :restaurants_menus, class_name: "RestaurantsMenu"
+  has_many :restaurants_menus, dependent: :destroy, class_name: "RestaurantsMenu"
   has_many :menus, through: :restaurants_menus
+  accepts_nested_attributes_for :menus
 
   validates :name, presence: true
 end
