028dbeb6a54d7358438e3b38f1d2535ab350e60b
Murilo Miranda <murilomirandamoreno@gmail.com>
2024-10-21 04:14:51 -0300

    Validate association
    - Add tests to validate presence and association

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Restaurant, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "associations" do
+    it { is_expected.to have_many(:menus) }
+  end
 end
